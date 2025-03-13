f92db0cb4d3a143a1579643e2d985009c897ed98
sazangg <younes.lesfer@gmail.com>
2025-03-13 00:51:33 +0000

    deployment

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+import { auth, signIn, signOut } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export default async function Navbar() {
+  const session = await auth();
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm ">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ?
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
+              </Link>
+            </>
+          : <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          }
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/StartupCard.tsx
@@ -0,0 +1,66 @@
+import { cn, formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+export default function StartupCard({ post }: { post: StartupTypeCard }) {
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup-card_date">{formatDate(post._createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{post.views}</span>
+        </div>
+      </div>
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${post.author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+          </Link>
+          <Link href={`/startup/${post._id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{post.title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${post.author?._id}`}>
+          <Image
+            src={post.author?.image!}
+            alt={post.author?.name!}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${post._id}`} className="flex flex-col gap-2">
+        <p className="startup-card_desc">{post.description}</p>
+        <img src={post.image} alt="placeholder" className="startup-card_img" />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${post.category?.toLowerCase()}`}>
+          <p className="text-16-medium">{post.category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${post._id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+}
+
+export const StartupCardSkeleton = () => {
+  return (
+    <>
+      {[0, 1, 2, 3, 4].map((index: number) => (
+        <li key={cn("skeleton", index)}>
+          <Skeleton className="startup-card_skeleton" />
+        </li>
+      ))}
+    </>
+  );
+};
